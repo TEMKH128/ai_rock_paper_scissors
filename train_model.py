1baae1b954bf8dbd927c76b725bc4137b6fae44c
Tebogo Mkhize <tebogomkhize01@gmail.com>
2024-02-15 12:13:27 +0200

    implement functionality to loads images from stored directories. Commit adds code to load images from specified directories, perform colour space conversion (BGR to RGB) and resize images (to 227 x 227 pixels), modified images are stored in a dataset for further processing and training

--- train_model.py
+++ train_model.py
@@ -1,9 +1,11 @@
+import os
+import cv2
 import keras.layers as layers
 from keras.models import Sequential
 from keras_squeezenet import SqueezeNet
 
 
-LABEL_MAP = {
+LABEL_MAP = {  # used also for num_classifications.
     "rock": 0,
     "paper": 1,
     "scissors": 2,
@@ -30,5 +32,33 @@ def retrieve_model(num_classifications):
 
     model = Sequential(linear_stack_layers)
     return model
+
+
+def load_collected_images(img_dir):
+    """
+    Retrieves images from image directory and resizes those images and
+    chang their colour space to RGB and stores each image in a list.
+    Parameters:
+      * img_dir: directory used to extract images it contains.
+    Return: dataset.
+    """
+    dataset = []
+    for directory in os.listdir(img_dir):
+        label_path = os.path.join(img_dir, directory)
+
+        if (not os.path.isdir(label_path)): continue
+
+        for item in os.listdir(label_path):
+            if (item.startswith(".")): continue  # excl. hidden files.
+
+            # read image from specified image path.
+            img = cv2.imread(os.path.join(label_path), item)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img = cv2.resize(img, (227, 227))  # 227 x 227 pixels, Squeezenet.
+            dataset.append([img, directory])
+
+    return dataset
+
+    
 
 
