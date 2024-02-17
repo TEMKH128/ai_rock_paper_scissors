fa7bc105962e579c11ea11f22d4f5158627551a6
Tebogo Mkhize <tebogomkhize01@gmail.com>
2024-02-17 17:47:34 +0200

    refactor: Squeezenet Base Model to DenseNet121
    Remove use of Squeezenet as base model in model due to import conflicts and use DenseNet 121.
    Remove imports related to Squeezenet and introduce those related to DenseNet121.
    Modify code to retrieve model to now use DenseNet121 as the base model and use different additional layers.
    change the size of images loaded from 227 x 227 pixels (SqueezeNet) to 224 x 224 (DenseNet).
    Modify code to train model to use a batch size of 8 and epochs of 5 (due to memory issues).
    Modify code that saves model to save file using native keras format instead of HDF5 file format.

--- train_model.py
+++ train_model.py
@@ -2,10 +2,10 @@ import os
 import cv2
 import numpy
 import keras.layers as layers
-from keras.utils import np_utils
 from keras.optimizers import Adam
 from keras.models import Sequential
-from keras_squeezenet import SqueezeNet
+from keras.utils import to_categorical
+from keras.applications.densenet import DenseNet121
 
 
 LABEL_MAP = {
@@ -21,21 +21,34 @@ def retrieve_model(num_classifications):
     Parameters:
       * num_classifications: number of classifications possible.
     Defines Convolution Neural Network (CNN) model architecture using
-    SqueezeNet as the base model (for feature extraction). Layers added
+    DenseNet121 as the base model (for feature extraction). Layers added
     to model and model (used for image classification) is returned.
     """
-    linear_stack_layers = [
-        SqueezeNet(input_shape=(227, 227, 3), include_top=False),
-        layers.Dropout(0.5),
-        layers.Convolution2D(num_classifications, (1, 1), padding='valid'),
-        layers.Activation('relu'),
-        layers.GlobalAveragePooling2D(),
-        layers.Activation('softmax')
-    ]
+    # DenseNet121 model used as base layer (include_top = false, don't incl.
+    # top classification layer), add our our own layers.
+    # weights="..." (pre-trained weights [parameters model learns during process] trained on ImageNet dataset will be loaded into
+    # model.
+    # classes=4: model trained to classify images into 4 classes.
+    # input_shape=... specifies shape of input images (224 x 224 pixels /w 3 colour channels)
+    # base_model.trainable=True allows fine-tuning of base model's weight during training.
+    # Sequential - keras model that creates a linear stack of layers (structure of neural network), layers added to sequential
+    # model are arranged sequentially (one after another) - output of 1 layer serves as input for next layer (flow of data from input layer to hidden layers to output layer)
+    # model.add(layers.MaxPool2D()) reduces spacial dimension of feature maps (shapes, texture, etc) helps reduce computational complexity and overfitting.
+    # layers.Flatten(): Flattens multi-dimensioanl output from previous layer into 1-dimensional vector, which is required before passing to a fully connected layer. [E.g. converting from grid to simple list]
+    # layers.Dense(num_classifications, activation='softmax') adds a Dense (fully connected) layer to the model with a softmax activation function.
+    # no. of units in layer = num._classification, softmax activation function to the output of the previous layer. Softmax converts the raw scores into probability distributions over the different classes.
 
-    model = Sequential(linear_stack_layers)
+    base_model = DenseNet121(include_top=False, weights='imagenet', classes=4, input_shape=(224, 224, 3))
+    base_model.trainable = True  # Set to False if you don't want to fine-tune
+
+    model = Sequential()
+    model.add(base_model)
+    model.add(layers.MaxPool2D())
+    model.add(layers.Flatten())
+    model.add(layers.Dense(num_classifications, activation='softmax'))
+    
     return model
-
+    
 
 def load_collected_images(img_dir):
     """
@@ -55,9 +68,9 @@ def load_collected_images(img_dir):
             if (item.startswith(".")): continue  # excl. hidden files.
 
             # read image from specified image path.
-            img = cv2.imread(os.path.join(label_path), item)
+            img = cv2.imread(os.path.join(label_path, item))
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            img = cv2.resize(img, (227, 227))  # 227 x 227 pixels, Squeezenet.
+            img = cv2.resize(img, (224, 224))  # 224 x 224 pixels, Squeezenet.
             dataset.append([img, directory])
 
     return dataset
@@ -93,7 +106,7 @@ def prepare_data(dataset):
     labels = list(map(mapper, labels))
 
     # One-hot encoding the labels.
-    labels = np_utils.to_categorical(labels)
+    labels = to_categorical(labels)
     # one hot encode the labels  # part of preparing the data.
     # Converts list of numerical labels into one-hot encoded vectors (binary vectors).
     # Each category is represented by a vector where all elements are 0 except for the index
@@ -125,7 +138,7 @@ def configure_model(model):
     # metrics - specifies metrics to be used for eval. model's performance during training and testing.
     # accuracy metric will be calc. and displayed during training.
     model.compile(
-        optimizer=Adam(lr=0.0001),
+        optimizer=Adam(learning_rate=0.0001),
         loss='categorical_crossentropy',
         metrics=['accuracy']
     )
@@ -143,14 +156,17 @@ def train_model(model, images, labels):
       * labels: one-hot encoded labels list that corresponds to images list.
     Return: The trained model.
     """
-    # why train over 10 epochs/iterations. The less the loss and higher the accuracy the better the model (goal of each epoch)
+    # why train over 8 epochs/iterations. The less the loss and higher the accuracy the better the model (goal of each epoch)
     # training for too few epochs may result in underfitting, where the model fails to capture the underlying patterns in the data.
     # Conversely, training for too many epochs may lead to overfitting, where the model performs 
     # well on the training data but fails to generalize to unseen data. 10 epochs choice is based on
     # empirical experimentation or prior knowledge of the dataset's complexity.
     # lists correspond with one another - index 0 of labels belong to index 0 of data.
     # model.fit() takes input data and its corresp. labels and does the training over a specified no. of epochs.
-    model.fit(numpy.array(images), numpy.array(labels), epochs=10)
+    # Batch size: no. of training examples (samples of data) used in one epoch/iteration, 
+    # using batches allows model to process data more efficiently, smaller batch sizes consume less memory.
+    # Epoch: 1 iteration or complete pass through entire training dataset (all batches per epoch), batches are randomised with each epoch. (To improve ability to generalise unseen data and avoid overfitting)
+    model.fit(numpy.array(images), numpy.array(labels), batch_size=8, epochs=5)
     return model
 
 
@@ -161,11 +177,11 @@ def save_trained_model(model):
     Parameters:
       * model: Trained model to be saved.
     """
-    model.save("trained_models/rock-paper-scissors-model.h5")
+    model.save("trained_models/rock-paper-scissors.keras")
     print("Model saved to trained_model directory within project.")
 
 
-def train_model():
+def execute_model_training():
     dataset = load_collected_images("image_data")
     images, labels = prepare_data(dataset)
     
@@ -176,4 +192,4 @@ def train_model():
 
 
 if __name__ == "__main__":
-    train_model()
+    execute_model_training()
